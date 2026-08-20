f6b05b8424a61e75d539420be491cbc103dcdcdf
DHernandezG2277 <dhernandezgarcia2277@gmail.com>
2026-08-20 13:40:07 -0500

    Add files via upload

---
+++ RpgManager/src/main/java/co/udistrital/edu/co/rpgmanager/RpgManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.udistrital.edu.co.rpgmanager;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class RpgManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
