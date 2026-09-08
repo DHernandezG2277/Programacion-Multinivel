16339a5b09c6b950cfa318ad34c86099821391f0
DHernandezG2277 <dhernandezgarcia2277@gmail.com>
2026-09-07 23:57:57 -0500

    Subida de la segunda gema de rpgmanager Java
    hubo complicaciones :(

--- rpgmanager/src/main/java/gestionrpg/App.java
+++ rpgmanager/src/main/java/gestionrpg/App.java
@@ -1,16 +1,54 @@
 package gestionrpg;
 
 import io.javalin.Javalin;
+import gestionrpg.modelo.Personaje;
 import gestionrpg.modelo.Repositorio;
 
 
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Repositorio repo = new Repositorio();
         var app = Javalin.create().start(7070);
         app.get("/personajes", ctx -> 
         {
-            ctx.json(repo.obtenerPersonajes());
+            String html = "<h1>Lista de Personajes</h1>";
+            html += "<table border='1'><tr><th>Nombre</th><th>Clase</th><th>Nivel</th><th>Vida</th></tr>";
+            for (Personaje p : repo.obtenerPersonajes()) {
+                html += "<tr>";
+                html += "<td>" + p.getNombre() + "</td>";
+                html += "<td>" + p.getClase() + "</td>";
+                html += "<td>" + p.getNivel() + "</td>";
+                html += "<td>" + p.getVida() + "</td>";
+                html += "</tr>";
+            }
+            html += "</table>";
+            html += "<form action= '/personajes' method='POST'>";
+                html += "<label>Nombre:</label>";
+                html += "<input type='text' name='nombre'>";
+                html += "<label>Clase:</label>";
+                html += "<input type='text' name='clase'>";
+                html += "<label>Nivel:</label>";
+                html += "<input type='text' name='nivel'>";
+                html += "<label>Vida:</label>";
+                html += "<input type='text' name='vida'>";
+                html += "<button type='submit'>Guardar Personaje</button>";
+            html += "</form>";
+            ctx.html(html);
         });
+        app.post("/personajes", ctx->
+        {
+            String nombre = ctx.formParam("nombre");
+            String clase = ctx.formParam("clase");
+            int nivel = Integer.parseInt(ctx.formParam("nivel"));
+            int vida = Integer.parseInt(ctx.formParam("vida"));
+            
+            Personaje nuevo = new Personaje(nombre, clase, nivel, vida);
+            repo.agregarPersonaje(nuevo);
+            
+            ctx.redirect("/personajes");
+        });
+            
     }
 }
--- rpgmanager/src/main/java/gestionrpg/modelo/Repositorio.java
+++ rpgmanager/src/main/java/gestionrpg/modelo/Repositorio.java
@@ -3,20 +3,25 @@ package gestionrpg.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Repositorio {
+public class Repositorio 
+{
     private List<Personaje> personajes;
-
-    public Repositorio() {
+    
+    public Repositorio() 
+    {
         this.personajes = new ArrayList<>();
         this.personajes.add(new Personaje("Aragorn", "Guerrero", 10, 100));
         this.personajes.add(new Personaje("Gandalf", "Mago", 15, 80));
     }
 
-    public void agregarPersonaje(Personaje personaje) {
+    public void agregarPersonaje(Personaje personaje) 
+    {
         personajes.add(personaje);
     }
 
-    public List<Personaje> obtenerPersonajes() {
+    public List<Personaje> obtenerPersonajes() 
+    {
         return personajes;
     }
+    
 }
