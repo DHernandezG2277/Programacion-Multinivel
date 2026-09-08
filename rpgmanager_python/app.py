4b9b3bc8bce93005655915209d9324181b5cd103
DHernandezG2277 <dhernandezgarcia2277@gmail.com>
2026-09-08 00:29:12 -0500

    Subida gema 2 de python
    Actualizacion de rpgManager en python, me parece mas confuso que java :(

--- rpgmanager_python/app.py
+++ rpgmanager_python/app.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template, request, redirect, url_for
 from flask import Flask, jsonify
 from Models.repositorio import Repositorio
 from Models.personaje import Personaje
@@ -7,10 +8,20 @@ repo=Repositorio()
 repo.agregar_personaje(Personaje("Aragorn", "Humano", "Guerrero", 20))
 repo.agregar_personaje(Personaje("Legolas", "Elfo", "Arquero", 18))
 
-@app.route('/personajes', methods=['GET'])
+@app.route('/personajes', methods=['GET', 'POST'])
 def get_personajes():
-    cambio= [personaje.Dictar() for personaje in repo.obtener_personajes()]
-    return jsonify(cambio)
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        raza = request.form['raza']
+        clase = request.form['clase']
+        nivel = int(request.form['nivel'])
+
+        nuevo_personaje = Personaje(nombre, raza, clase, nivel)
+        repo.agregar_personaje(nuevo_personaje)
+
+        return redirect(url_for('get_personajes'))
+    
+    return render_template('personajes.html', personajes= repo.obtener_personajes())
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
